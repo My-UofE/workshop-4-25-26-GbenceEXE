c1ae44d799072b6bd2bb2cb6f29ddb71220cf7f8
Bence Gallai <bgg203@exeter.ac.uk>
2026-02-13 08:34:41 +0000

    completed part 5

--- Rectangle.java
+++ Rectangle.java
@@ -25,12 +25,12 @@ public class Rectangle {
     public Rectangle(double width, double height) {
         this(width, height, 0, 0);
     }
-
+    
     // third constructor:
     public Rectangle() {
         this(1, 1, 0, 0);
     }
-
+    
     public double getWidth(){return width;}
     public double getHeight(){return height;}
     public double getOriginX(){return originX;}
@@ -41,18 +41,21 @@ public class Rectangle {
             this.width = width;
         }
     }
-
     public void setHeight(double height){
         if (height > 0) {
             this.height = height;
         }
     }
-
-
     public void setOriginX(double originX){this.originX = originX;}
     public void setOriginY(double originY){this.originY = originY;}
-
-
+    
+    // Return a description of a rectangle in the form of
+    // Rectangle[x=*,y=*,w=*,h=*]
+    public String toString(){
+        return "Rectangle[x=" + originX + ", y=" + originY + ", w=" + width + ", h=" + height + "]";
+    }
+    
+    
     // method: move the rectangle
     public void move(double dx, double dy) {
         originX += dx;
@@ -73,12 +76,12 @@ public class Rectangle {
         width *= scaleX;
         height *= scaleY;
     }
-
+    
     public void scale(double scale) {
         width *= scale;
         height *= scale;
     }
-
+    
     public boolean isOverlappedWith(Rectangle r) {
         double rLeft = r.originX;
         double rRight = r.originX + r.width;
@@ -88,31 +91,31 @@ public class Rectangle {
         double thisRight = this.originX + this.width;
         double thisTop = this.originY;
         double thisBottom = this.originY + this.height;
-
+        
         boolean isOverlappedLeft = rLeft >= thisLeft && rLeft <= thisRight;
         boolean isOverlappedRight = rRight >= thisLeft && rRight <= thisRight;
         boolean isOverlappedTop = rTop >= thisTop && rTop <= thisBottom;        
         boolean isOverlappedBottom = rBottom >= thisTop && rBottom <= thisBottom;
-
+        
         boolean isFullyInside = (rLeft < thisLeft) && (rRight > thisRight) && (rTop < thisTop) && (rBottom > thisBottom);
-
+        
         boolean isOverlapped = ((isOverlappedLeft || isOverlappedRight) && (isOverlappedTop || isOverlappedBottom)) || isFullyInside;
-
+        
         return isOverlapped;
     }
-
+    
     public static boolean areOverlapping(Rectangle r1, Rectangle r2){
         return r1.isOverlappedWith(r2);
     }
-
+    
     public double calcRatio(){
         return this.width / this.height;
     }
-
+    
     public boolean isSquare(){
         final int SQUARE_RATIO = 1;
         final double EPSILON = 0.001;
-
+        
         return Math.abs(SQUARE_RATIO - this.calcRatio()) < EPSILON;
     }
 }
