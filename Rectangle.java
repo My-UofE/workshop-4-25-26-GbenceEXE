ef1e5f3723f83a3d2bf816c9462a21d2f07f99ea
Bence Gallai <bgg203@exeter.ac.uk>
2026-02-13 05:16:47 +0000

    completed part 1

--- Rectangle.java
+++ Rectangle.java
@@ -1,37 +1,47 @@
 // A Rectangle class
 public class Rectangle {
-  
-  // 4 instance attributes
-  public double width;
-  public double height;
-  public double originX = 0.0;
-  public double originY = 0.0;
+    
+    // 4 instance attributes
+    public double width;
+    public double height;
+    public double originX = 0.0;
+    public double originY = 0.0;
+    
+    // 1 static attributes
+    public static final int NUMBER_OF_SIDES = 4;
+    
+    // main constructor
+    public Rectangle(double width, double height, double originX, double originY) {
+        this.width = width;
+        this.height = height;
+        this.originX = originX;
+        this.originY = originY;
+    }
+    
+    // second constructor: 
+    public Rectangle(double width, double height) {
+        this(width, height, 0, 0);
+    }
 
-  // 1 static attributes
-  public static final int NUMBER_OF_SIDES = 4;
-
-  // main constructor
-  public Rectangle(double w, double h, double oX, double oY) {
-    width = w;
-    height = h;
-    originX = oX;
-    originY = oY;
-  }
-
-  // method: move the rectangle
-  public void move(double dx, double dy) {
-	   originX += dx;
-	   originY += dy;
-  }
-
-  // method: compute the area of the rectangle
-  public double getArea() {
-    return width * height;
-  }
-  
-  // method: compute the area of the rectangle
-  public double getPerimeter() {
-    return 2 * (width + height);
-  }
-  
+    // third constructor:
+    public Rectangle() {
+        this(1, 1, 0, 0);
+    }
+    
+    // method: move the rectangle
+    public void move(double dx, double dy) {
+        originX += dx;
+        originY += dy;
+    }
+    
+    // method: compute the area of the rectangle
+    public double getArea() {
+        return width * height;
+    }
+    
+    // method: compute the area of the rectangle
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+    
 }
